3976cdb5dc2ae81540cf33d62d582e22ba0d513b
Azqiadistya <azqiadistya.aa@gmail.com>
2024-07-16 10:38:22 +0700

    add:update

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Auth; // Pastikan facade Auth diimpor
+use Illuminate\Support\Facades\Auth; 
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
 
